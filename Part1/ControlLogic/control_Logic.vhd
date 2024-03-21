8f3ed4372fccf4b958b92d42c33468f011b42931
Zaba821 <114114833+Zaba821@users.noreply.github.com>
2024-03-20 19:13:39 -0500

    new control logic

--- Part1/ControlLogic/control_Logic.vhd
+++ Part1/ControlLogic/control_Logic.vhd
@@ -4,6 +4,7 @@
 -- Iowa State University
 -------------------------------------------------------------------------
 
+
 -- control_Logic.vhd
 -------------------------------------------------------------------------
 -- DESCRIPTION: This file contains an implementation of a test bench for 
@@ -11,7 +12,6 @@
 --
 -- NOTES:
 -- 2/28/24
--- File finished - 3/6/24 
 -------------------------------------------------------------------------
 
 library IEEE;
@@ -21,53 +21,84 @@ library std;
 use std.env.all; -- For hierarchical/external signals
 use std.textio.all; -- For basic I/O
 
+
+
+
+
 entity control_Logic is 
    port ( 
          i_DOpcode : in std_logic_vector(5 downto 0);
          i_DFunc : in std_logic_vector(5 downto 0);
-         o_signals : out std_logic_vector(17 downto 0));
+         o_signals : out std_logic_vector(21 downto 0));
       
 end control_Logic;
 
 
+
+
+
 architecture flow of control_Logic is
+--signals 
 
-      -- Dont care is a 0, dont forget!!
+begin  
+ 
+ 
+ 
 
-      begin  
- 	o_signals <= "100011000000000110" when i_DOpcode = "001000" else                          --                    | addi
-                   "000010000000000110" when i_DOpcode = "000000" and i_DFunc = "010100" else   --                    | add
-                   "100011000000000010" when i_DOpcode = "001001" else                          --                    | addiu
-                   "100010000000000010" when i_DOpcode = "000000" and i_DFunc = "010101" else   -- 1000100000000000X0 | addu
-                   "000010000000001000" when i_DOpcode = "000000" and i_DFunc = "011000" else   -- 0X00100000000010X0 | and
-                   "100011000000001000" when i_DOpcode = "001100" else                          -- 1X0011000000001000 | andi
-	             "000011000000010000" when i_DOpcode = "001111" else                          -- XX0011000000010000 | lui
-  		       "101011000000000010" when i_DOpcode = "100111" else                          -- 101011000000000010 | lw
-		       "000010000000011000" when i_DOpcode = "000000" and i_DFunc = "100111" else   -- XX00100000000110X0 | nor
-                   "000010000000100000" when i_DOpcode = "000000" and i_DFunc = "100110" else   -- XX00100000001000X0 | xor
-	             "100011000000100000" when i_DOpcode = "001110" else                          -- 1X0011000000100000 | xori
-                   "000010000000101000" when i_DOpcode = "000000" and i_DFunc = "100101" else   -- XX00100000001010X0 | or
-                   "100011000000101000" when i_DOpcode = "001101" else                          -- 1X0011000000101000 | ori
-                   "010010000000110000" when i_DOpcode = "000000" and i_DFunc = "101010" else   -- 0100100000001100X0 | slt
-                   "110011000000110010" when i_DOpcode = "001010" else                          -- 110011000000110010 | slti
-                   "000010000000111000" when i_DOpcode = "000000" and i_DFunc = "000000" else   -- 000010000000111000 | sll (exluding dont cares)
-                   "000010000001000000" when i_DOpcode = "000000" and i_DFunc = "000010" else   -- XX00100000010000X0 | srl
-                   "000010000001001000" when i_DOpcode = "000000" and i_DFunc = "000011" else   -- XX00100000010010X0 | sra
-                   "100100000000000010" when i_DOpcode = "101011" else                          -- 10010X000000000010 | sw
-                   "010010000000000100" when i_DOpcode = "000000" and i_DFunc = "100010" else   -- 0100100000000001X0 | sub
-                   "010010000000000000" when i_DOpcode = "000000" and i_DFunc = "100011" else   -- 0100100000000000X0 | subu
-                   "010000100000000000" when i_DOpcode = "000100" else                          -- 01000X1000000000X0 | beq
-                   "010000010000000000" when i_DOpcode = "000101" else                          -- 01000X0100000000X0 | bne
-                   "000000001000000000" when i_DOpcode = "000010" else                          -- XX000X00100XXXX0X0 | j
-                   "000010001100000000" when i_DOpcode = "000011" else                          -- XX001X(R31)00110XXXX0X0 | jal
-                   "000000000010000000" when i_DOpcode = "000000" and i_DFunc = "001000" else   -- XX000X00001XXXX0X0 | jr
-                   "101011000000000010" when i_DOpcode = "100000" else                          -- 101011000000000010 | lb
-                   "101011000000000010" when i_DOpcode = "100001" else                          -- 101011000000000010 | lh
-                   "101011000000000000" when i_DOpcode = "100100" else                          -- 101011000000000000 | lbu
-                   "101011000000000000" when i_DOpcode = "100101" else                          -- 101011000000000000 | lhu
-                   "000010000000111001" when i_DOpcode = "000000" and i_DFunc = "000100" else   -- XX00100000001110X1 | sllv
-                   "000010000001000001" when i_DOpcode = "000000" and i_DFunc = "000110" else   -- XX00100000010000X1 | srlv
-                   "000010000001001001" when i_DOpcode = "000000" and i_DFunc = "000111" else   -- XX00100000010010X1 | srav
-                   "000000000000000000";
+ 	o_signals <= "1000110000000001100000" when i_DOpcode = "001000" else -- addi
+                     "0000100000000001100000" when i_DOpcode = "000000" and i_DFunc = "010100" else -- add
+                     "1000110000000000100000" when i_DOpcode = "001001" else -- addiu
+                     "1000100000000000100000" when i_DOpcode = "000000" and i_DFunc = "010101" else --  1000100000000000X0 addu
+                     "0000100000000010000000" when i_DOpcode = "000000" and i_DFunc = "011000" else --  0X00100000000010X0 and
+                     "1000110000000010000000" when i_DOpcode = "001100" else -- 1X0011000000001000 andi
+		     "0000110000000100000000" when i_DOpcode = "001111" else -- XX0011000000010000 lui
+  		     "1010110000000000100000" when i_DOpcode = "100111" else -- 101011000000000010 lw
+		     "0000100000000110000000" when i_DOpcode = "000000" and i_DFunc = "100111" else --XX00100000000110X0 nor
+                     "0000100000001000000000" when i_DOpcode = "000000" and i_DFunc = "100110" else --XX00100000001000X0 xor
+		     "1000110000001000000000" when i_DOpcode = "001110" else -- 1X0011000000100000 xori
+                     "0000100000001010000000" when i_DOpcode = "000000" and i_DFunc = "100101" else -- XX00100000001010X0 or
+                     "1000110000001010000000" when i_DOpcode = "001101" else -- 1X0011000000101000 ori
+                     "0100100000001100000000" when i_DOpcode = "000000" and i_DFunc = "101010" else -- 0100100000001100X0 slt
+                     "1100110000001100100000" when i_DOpcode = "001010" else -- 110011000000110010 slti
+                     "0000100000001110000000" when i_DOpcode = "000000" and i_DFunc = "000000" else -- 000010000000111000 sll (exluding dont cares)
+                     "0000100000010000000000" when i_DOpcode = "000000" and i_DFunc = "000010" else -- XX00100000010000X0 srl
+                     "0000100000010010000000" when i_DOpcode = "000000" and i_DFunc = "000011" else -- XX00100000010010X0 sra
+                     "1001000000000000100000" when i_DOpcode = "101011" else -- 10010X000000000010 sw
+                     "0100100000000001000000" when i_DOpcode = "000000" and i_DFunc = "100010" else -- 0100100000000001X0 sub
+                     "0100100000000000000000" when i_DOpcode = "000000" and i_DFunc = "100011" else -- 0100100000000000X0 subu
+                     "0100001000000000000000" when i_DOpcode = "000100" else -- 01000X1000000000X0 beq
+                     "0100000100000000000000" when i_DOpcode = "000101" else -- 01000X0100000000X0 bne
+                     "0000000010000000000000" when i_DOpcode = "000010" else -- XX000X00100XXXX0X0 j
+                     "0000100011000000000000" when i_DOpcode = "000011" else -- XX001X(R31)00110XXXX0X0 jal
+                     "0000000000100000000000" when i_DOpcode = "000000" and i_DFunc = "001000" else -- XX000X00001XXXX0X0 jr
+                     "1010110000000000100010" when i_DOpcode = "100000" else -- 101011000000000010 lb
+                     "1010110000000000100110" when i_DOpcode = "100001" else --101011000000000010 lh
+                     "1010110000000000000100" when i_DOpcode = "100100" else -- 101011000000000000 lbu
+                     "1010110000000000001000" when i_DOpcode = "100101" else -- 101011000000000000 lhu
+                     "0000100000001110010000" when i_DOpcode = "000000" and i_DFunc = "000100" else -- XX00100000001110X1 sllv
+                     "0000100000010000010000" when i_DOpcode = "000000" and i_DFunc = "000110" else -- XX00100000010000X1 srlv
+                     "0000100000010010010000" when i_DOpcode = "000000" and i_DFunc = "000111" else -- XX00100000010010X1 srav
+                     "0000000000000000000001" when i_DOpcode = "010100" else -- 0000000000000000000001 halt
+                     "0000000000000000000000";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end flow;
--- Part1/ControlLogic/tb_control_Logic.vhd
+++ Part1/ControlLogic/tb_control_Logic.vhd
@@ -4,6 +4,7 @@
 -- Iowa State University
 -------------------------------------------------------------------------
 
+
 -- tb_control_Logic.vhd
 -------------------------------------------------------------------------
 -- DESCRIPTION: This file contains an implementation of a test bench for 
@@ -11,289 +12,90 @@
 --
 -- NOTES:
 -- 2/29/24
--- Finished testing 3/6/24 - Camden Fergen
 -------------------------------------------------------------------------
 
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.ALL;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-ENTITY tb_control_Logic IS
-  GENERIC (
-    gCLK_HPER : TIME := 10 ns;
-    N : INTEGER := 32);
-END tb_control_Logic;
+entity tb_control_Logic is
+  generic(gCLK_HPER   : time := 50 ns; N : integer := 32);
+end tb_control_Logic;
 
-ARCHITECTURE behavior OF tb_control_Logic IS
-
+architecture behavior of tb_control_Logic is
+  
   -- Calculate the clock period as twice the half-period
-  CONSTANT cCLK_PER : TIME := gCLK_HPER * 2;
-  COMPONENT control_logic
-    PORT (
-      i_DOpcode : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
-      i_DFunc : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
-      o_signals : OUT STD_LOGIC_VECTOR(17 DOWNTO 0));
+  constant cCLK_PER  : time := gCLK_HPER * 2;
 
-  END COMPONENT;
+
+  component control_logic
+    port(
+        i_DOpcode : in std_logic_vector(5 downto 0);
+         i_DFunc : in std_logic_vector(5 downto 0);
+         o_signals : out std_logic_vector(21 downto 0));
+
+  end component;
 
   -- Temporary signals to connect to the dff component.
-  SIGNAL s_opcode : STD_LOGIC_VECTOR(5 DOWNTO 0);
-  SIGNAL s_Dfunc : STD_LOGIC_VECTOR(5 DOWNTO 0);
-  SIGNAL s_signalsOut : STD_LOGIC_VECTOR(17 DOWNTO 0);
+  signal s_opcode : std_logic_vector(5 downto 0);
+  signal s_func : std_logic_vector(5 downto 0);
+  signal s_signalsOut : std_logic_vector(21 downto 0);
 
-BEGIN
+begin
 
-  DUT : control_logic
-  PORT MAP(
-    i_DOpcode => s_opcode,
-    i_DFunc => s_Dfunc,
-    o_signals => s_signalsOut);
-  P_TEST_CASES : PROCESS
-  BEGIN
+  DUT: control_logic 
+  port map(i_DOpcode => s_opcode, 
+           i_DFunc => s_func,
+           o_signals => s_signalsOut);
 
+ 
+P_TEST_CASES: process
+  begin
     -- Test cases:
+    
 
-    -- Testing addi - basic
-    s_opcode <= "001000";
-    -- Expecting: 100011000000000110
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000000110") report "Basic addi failed" severity error;
-
-    -- Testing addi - random dfunc
-    s_opcode <= "001000";
-    s_dfunc  <= "000100";
-    -- Expecting: 100011000000000110
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000000110") report "Random dfunc addi failed" severity error;
+    s_opcode  <= "000000";  
+    s_func  <= "010100";  
+  
+    wait for gCLK_HPER*2;
 
 
-    -- Testing add - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc  <= "010100";
-    -- Expecting: 000010000000000110
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000000110") report "add correct dfunc failed" severity error;
+    s_opcode  <= "001110";  
+    s_func  <= "010100";  -- num should not matter
+
+    wait for gCLK_HPER*2;
 
 
-    -- Testing addiu - basic
-    s_opcode <= "001001";
-    -- Expecting: 100011000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000000010") report "addiu basic failed" severity error;
+    s_opcode  <= "000100";  
+    s_func  <= "111111";  -- num should not matter
 
 
-    -- Testing addiu - radnom dfunc
-    s_opcode <= "001001";
-    s_dfunc  <= "010101";
-    -- Expecting: 100011000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000000010") report "addiu random dfunc failed" severity error;
+   wait for gCLK_HPER*2;
 
-    -- Testing addu - basic
-    s_opcode <= "000000";
-    s_dfunc  <= "010101";
-    -- Expecting: 100010000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="100010000000000010") report "addu basic failed" severity error;
+    s_opcode  <= "000000";  
+    s_func  <= "001000";  
 
-    -- Testing and - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "011000";
-    -- Expecting: 000010000000001000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000001000") report "and correct dfunc failed" severity error;
+    wait for gCLK_HPER*2;
 
-    -- Testing andi - basic
-    s_opcode <= "001100";
-    -- Expecting: 100011000000001000
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000001000") report "andi basic failed" severity error;
+    s_opcode  <= "000000";  
+    s_func  <= "000111";  
 
-    -- Testing lui - basic
-    s_opcode <= "001111";
-    -- Expecting: 000011000000010000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000011000000010000") report "lui basic failed" severity error;
+   wait for gCLK_HPER*2;
 
-    -- Testing lw - basic
-    s_opcode <= "100111";
-    -- Expecting: 101011000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="101011000000000010") report "lw basic failed" severity error;
-
-    -- Testing nor - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "100111";
-    -- Expecting: 000010000000011000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000011000") report "nor correct dfunc failed" severity error;
-
-    -- Testing xor - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "100110";
-    -- Expecting: 000010000000100000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000100000") report "xor correct dfunc failed" severity error;
-
-    -- Testing xori - basic
-    s_opcode <= "001110";
-    -- Expecting: 100011000000100000
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000100000") report "xori basic failed" severity error;
-
-    -- Testing or - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "100101";
-    -- Expecting: 000010000000101000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000101000") report "or correct dfunc failed" severity error;
-
-    -- Testing ori - basic
-    s_opcode <= "001101";
-    -- Expecting: 100011000000101000
-    wait for cClk_per*2;
-    assert (s_signalsOut="100011000000101000") report "ori basic failed" severity error;
-
-    -- Testing slt - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "101010";
-    -- Expecting: 010010000000110000
-    wait for cClk_per*2;
-    assert (s_signalsOut="010010000000110000") report "slt correct dfunc failed" severity error;
-
-    -- Testing slti - basic
-    s_opcode <= "001010";
-    -- Expecting: 110011000000110010
-    wait for cClk_per*2;
-    assert (s_signalsOut="110011000000110010") report "slti basic failed" severity error;
-
-    -- Testing sll (excluding don't cares) - basic
-    s_opcode <= "000000";
-    s_dfunc <= "000000";
-    -- Expecting: 000010000000111000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000111000") report "sll (excluding don't cares) basic failed" severity error;
-
-    -- Testing srl - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "000010";
-    -- Expecting: 000010000001000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000001000000") report "srl correct dfunc failed" severity error;
-
-    -- Testing sra - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "000011";
-    -- Expecting: 000010000001001000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000001001000") report "sra correct dfunc failed" severity error;
-
-    -- Testing sw - basic
-    s_opcode <= "101011";
-    -- Expecting: 100100000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="100100000000000010") report "sw basic failed" severity error;
-
-    -- Testing sub - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "100010";
-    -- Expecting: 010010000000000100
-    wait for cClk_per*2;
-    assert (s_signalsOut="010010000000000100") report "sub correct dfunc failed" severity error;
-
-    -- Testing subu - basic
-    s_opcode <= "000000";
-    s_dfunc <= "100011";
-    -- Expecting: 010010000000000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="010010000000000000") report "subu basic failed" severity error;
-
-    -- Testing beq - basic
-    s_opcode <= "000100";
-    -- Expecting: 010000100000000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="010000100000000000") report "beq basic failed" severity error;
-
-    -- Testing bne - basic
-    s_opcode <= "000101";
-    -- Expecting: 010000010000000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="010000010000000000") report "bne basic failed" severity error;
-
-    -- Testing j - basic
-    s_opcode <= "000010";
-    -- Expecting: 000000001000000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000000001000000000") report "j basic failed" severity error;
-
-    -- Testing jal - basic
-    s_opcode <= "000011";
-    -- Expecting: 000010001100000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010001100000000") report "jal basic failed" severity error;
-
-    -- Testing jr - basic
-    s_opcode <= "000000";
-    s_dfunc <= "001000";
-    -- Expecting: 000000000010000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="000000000010000000") report "jr basic failed" severity error;
-
-    -- Testing lb - basic
-    s_opcode <= "100000";
-    -- Expecting: 101011000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="101011000000000010") report "lb basic failed" severity error;
-
-    -- Testing lh - basic
-    s_opcode <= "100001";
-    -- Expecting: 101011000000000010
-    wait for cClk_per*2;
-    assert (s_signalsOut="101011000000000010") report "lh basic failed" severity error;
-
-    -- Testing lbu - basic
-    s_opcode <= "100100";
-    -- Expecting: 101011000000000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="101011000000000000") report "lbu basic failed" severity error;
-
-    -- Testing lhu - basic
-    s_opcode <= "100101";
-    -- Expecting: 101011000000000000
-    wait for cClk_per*2;
-    assert (s_signalsOut="101011000000000000") report "lhu basic failed" severity error;
-
-    -- Testing sllv - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "000100";
-    -- Expecting: 000010000000111001
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000000111001") report "sllv correct dfunc failed" severity error;
-
-    -- Testing srlv - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "000110";
-    -- Expecting: 000010000001000001
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000001000001") report "srlv correct dfunc failed" severity error;
-
-    -- Testing srav - correct dfunc
-    s_opcode <= "000000";
-    s_dfunc <= "000111";
-    -- Expecting: 000010000001001001
-    wait for cClk_per*2;
-    assert (s_signalsOut="000010000001001001") report "srav correct dfunc failed" severity error;
-
-    -- Testing random
-    s_opcode <= "101010";
-    s_dfunc <= "000111";
-    -- Expecting: 000010000001001001
-    wait for cClk_per*2;
-    assert (s_signalsOut="000000000000000000") report "random test failed" severity error;
+    s_opcode  <= "111111";  -- no opcode has this
+    s_func  <= "000111";  
 
 
-    report "Testbench of control logic completely successfully!" severity note;
+   wait for gCLK_HPER*2;
 
-    WAIT;
-  END PROCESS;
+    s_opcode  <= "100001";  -- no opcode has this
+    s_func  <= "000111";  
 
-END behavior;
+   
+-- expecting: 000010000000000110 (add), 100011000000100000 xori, 010000100000000000 (beq), 000000000010000000 (jr), 000010000001001001 (srav), 000000000000000000 (nothing)
+
+wait;
+  end process;
+
+
+  
+end behavior;
