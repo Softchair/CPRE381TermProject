b6a46cec5294fcabe924509879ec0ca6c2337173
Zaba821 <114114833+Zaba821@users.noreply.github.com>
2024-03-25 15:45:05 -0500

    updated fetch logic

--- cpre381-toolflow/proj/src/TopLevel/FetchLogic/fetch_Logic.vhd
+++ cpre381-toolflow/proj/src/TopLevel/FetchLogic/fetch_Logic.vhd
@@ -251,14 +251,14 @@ architecture mixed of fetch_logic is
             generic map(N => 32)
             port map(
                 i_A     => s_JumpAddMuxOut, -- From output of jump mux (0)
-                i_B     => i_JReg, -- Jump register output (1)
+                i_B     => s_JumpAddMuxOut, -- Jump register output (1)
                 i_Sel   => i_JRegLogic, -- From control
                 o_Out   => s_JumpRegMuxOut -- To PC reg
             );
 
         -- Also assign the out of PC address to the result of jump reg control
         o_PCAddress <= s_PCaddressOut;
-
+        o_jalAdd  <= s_PCNext; -- NEW
         -- ------- END ENDING LOGIC CONTROL ------- --
 
 end mixed;
