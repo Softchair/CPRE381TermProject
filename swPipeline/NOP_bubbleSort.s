d32402ea393ee1938b4fb10fee4d92e7c9db9383
Softchair <24703877+Softchair@users.noreply.github.com>
2024-04-10 16:01:13 -0500

    updated nop bubble sort

--- swPipeline/NOP_bubbleSort.s
+++ swPipeline/NOP_bubbleSort.s
@@ -7,15 +7,23 @@ space: .asciiz " "
 main:
     #la $s0, array # Load the address of the array into $s0
     lui $1, 0x00001001 #non psuedo
+    NOP
+    NOP 
+    NOP
     ori $16, $1, 0x00000000
+   
     addiu $8 $0, 0x00000000 #li $t0, 0      # Initialize i = 0
+
     addiu $9, $0, 0x00000000 #li $t1, 0      # Initialize j = 0
+ 
     addiu $17, $0, 0x0000000b #li $s1, 11     # Set the array length (11 elements)
+
     addiu $18, $0, 0x0000000b #li $s2, 11     # Initialize n-i for inner loop
-
+ 
     add $t2, $zero, $s0 # For iterating address by i
+ 
     add $t3, $zero, $s0 # For iterating address by j
-
+ 
     addi $s1, $s1, -1
 
 outer_loop:
@@ -25,12 +33,10 @@ outer_loop:
     nop
     nop
     nop
-    nop
 
 inner_loop:
     lw $s3, 0($t3) # Load array[j] into $s3
     addi $t3, $t3, 4 # Increment address for j
-    nop
     nop
     nop
     nop
@@ -43,40 +49,29 @@ inner_loop:
     nop
     nop
     nop
-    nop
     bne $t4, $zero, cond # If $s3 < $s4, skip swap
-    nop
-
+    NOP
 swap:
     sw $s3, 0($t3) # Swap array[j] and array[j+1]
     sw $s4, -4($t3)
-    nop
-    nop
-    nop
-    nop
+
     lw $s4, 0($t3)
 
 cond:
     bne $t1, $s2, inner_loop # If j != n-i, continue inner loop
-    nop
-
+    NOP 
     addi $t0, $t0, 1 # Increment i
     nop
     nop
     nop
-    nop
     bne $t0, $s1, outer_loop # If i != n, continue outer loop
-    nop
-    nop
-    nop
     nop
     li $t0, 1
 
 print_loop:
     addiu $2, $0, 0x00000001 #li $v0, 1
     lw $a0, 0($t2)
-    nop
-    nop
+
     syscall
     addiu $2, $0, 0x00000004 #li $v0, 4
     lui $1, 0x00001001 #la $a0, space
@@ -88,9 +83,7 @@ print_loop:
     nop
     nop
     nop
-    nop
     bne $t0, $s1, print_loop # If i != n, continue print loop
     nop
-
 exit:
     halt
