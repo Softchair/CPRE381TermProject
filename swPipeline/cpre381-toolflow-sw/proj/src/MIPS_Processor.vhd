86d45dac57509fdcc04cc3998c52edd50fc654b6
Softchair <24703877+Softchair@users.noreply.github.com>
2024-04-29 16:55:07 -0500

    merging

--- swPipeline/cpre381-toolflow-sw/proj/src/MIPS_Processor.vhd
+++ swPipeline/cpre381-toolflow-sw/proj/src/MIPS_Processor.vhd
@@ -531,17 +531,10 @@ subtractor : adderSubs
   o_Cout => open);
  
 
-<<<<<<< HEAD
-     g_orG32 : orG32b
-     port MAP(
-      i_D => s_subtractorOut,
-      o_Out        => s_orGateZeroOut);
-=======
 g_orG32 : orG32b
   port MAP(
     i_D => s_subtractorOut,
     o_Out        => s_orGateZeroOut);
->>>>>>> 373a7808e29b632bf2a1faeb3988b4d3cf6ff6fd
    
    
   g_invg : invg 
